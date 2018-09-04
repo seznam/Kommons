0acb279284224eddbd9d5ef7a9c547a850bed77a
Jakub Janda <jakub.janda@firma.seznam.cz>
2018-09-04 23:21:54 +0200

    ADD: Access operator on SparseArray

--- kommons-core/src/main/java/cz/seznam/kommons/kexts/SparseArrayExtensions.kt
+++ kommons-core/src/main/java/cz/seznam/kommons/kexts/SparseArrayExtensions.kt
@@ -7,3 +7,8 @@ import android.util.SparseArray
  */
 inline val <reified T> SparseArray<T>.values: Array<T>
 	get() = Array(this.size()) { this.valueAt(it) }
+
+operator fun <T> SparseArray<T>.set(key: Int, value: T) {
+	put(key, value)
+}
+
