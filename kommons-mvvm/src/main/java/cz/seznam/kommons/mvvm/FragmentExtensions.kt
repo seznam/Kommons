af67f5d3c27c06aa586760743fa822d115248191
Jakub Janda <jakub.janda@firma.seznam.cz>
2019-10-23 10:29:51 +0200

    ADD: Extenstion func creating Viewmodel for fragment with SavedStateHandle with default params

--- kommons-mvvm/src/main/java/cz/seznam/kommons/mvvm/FragmentExtensions.kt
+++ kommons-mvvm/src/main/java/cz/seznam/kommons/mvvm/FragmentExtensions.kt
@@ -1,5 +1,6 @@
 package cz.seznam.kommons.mvvm
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 
@@ -25,4 +26,21 @@ inline fun <reified V : ViewModel> Fragment.obtainViewModelWithState(crossinline
       return viewModelFactory(handle) as V
     }
   }).get(V::class.java)
+}
+
+inline fun <reified V : ViewModel> Fragment.obtainViewModelWithState(
+  defaultArgs: Bundle?,
+  crossinline viewModelFactory: (savedStateHandle: SavedStateHandle) -> V
+): V {
+  return ViewModelProviders.of(
+    this,
+    object : AbstractSavedStateViewModelFactory(this, defaultArgs) {
+      override fun <V : ViewModel?> create(
+        key: String,
+        modelClass: Class<V>,
+        handle: SavedStateHandle
+      ): V {
+        return viewModelFactory(handle) as V
+      }
+    }).get(V::class.java)
 }
