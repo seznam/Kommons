c2424df77deb0e2a42abb605c526423f22eba133
Jakub Janda <jakub.janda@firma.seznam.cz>
2019-10-30 09:55:27 +0100

    MOD: SDK 29 update

--- kommons-mvvm/src/main/java/cz/seznam/kommons/mvvm/FragmentExtensions.kt
+++ kommons-mvvm/src/main/java/cz/seznam/kommons/mvvm/FragmentExtensions.kt
@@ -2,14 +2,17 @@ package cz.seznam.kommons.mvvm
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.*
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 /**
  * @author Jakub Janda
  */
 
 inline fun <reified V : ViewModel> Fragment.obtainViewModel(crossinline viewModelFactory: () -> V): V {
-  return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+  return ViewModelProvider(this, object : ViewModelProvider.Factory {
     override fun <V : ViewModel?> create(modelClass: Class<V>): V {
       return viewModelFactory() as V
     }
@@ -17,7 +20,7 @@ inline fun <reified V : ViewModel> Fragment.obtainViewModel(crossinline viewMode
 }
 
 inline fun <reified V : ViewModel> Fragment.obtainViewModelWithState(crossinline viewModelFactory: (savedStateHandle: SavedStateHandle) -> V): V {
-  return ViewModelProviders.of(this, object : AbstractSavedStateViewModelFactory(this, null) {
+  return ViewModelProvider(this, object : AbstractSavedStateViewModelFactory(this, null) {
     override fun <V : ViewModel?> create(
       key: String,
       modelClass: Class<V>,
@@ -32,15 +35,13 @@ inline fun <reified V : ViewModel> Fragment.obtainViewModelWithState(
   defaultArgs: Bundle?,
   crossinline viewModelFactory: (savedStateHandle: SavedStateHandle) -> V
 ): V {
-  return ViewModelProviders.of(
-    this,
-    object : AbstractSavedStateViewModelFactory(this, defaultArgs) {
-      override fun <V : ViewModel?> create(
-        key: String,
-        modelClass: Class<V>,
-        handle: SavedStateHandle
-      ): V {
-        return viewModelFactory(handle) as V
-      }
-    }).get(V::class.java)
+  return ViewModelProvider(this, object : AbstractSavedStateViewModelFactory(this, defaultArgs) {
+    override fun <V : ViewModel?> create(
+      key: String,
+      modelClass: Class<V>,
+      handle: SavedStateHandle
+    ): V {
+      return viewModelFactory(handle) as V
+    }
+  }).get(V::class.java)
 }
